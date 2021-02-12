241ea51f487a9b85cdf65556ca4e82a31b0466f2
josiah.wai <jwai@princeton.edu>
2021-02-12 11:32:28 -0500

    Modify startup files to run on portal

--- startup_portal.m
+++ startup_portal.m
@@ -1,5 +1,5 @@
 restoredefaultpath
-RAMPROOT = '/Users/jwai/Research/rampup_nstxu/';
+RAMPROOT = '/u/jwai/rampup_nstxu/';
 addpath(genpath(RAMPROOT));
 setenv('RAMPROOT', RAMPROOT);
 
@@ -7,6 +7,7 @@ setenv('RAMPROOT', RAMPROOT);
 % Remove from path, any files in a directory labeled 'old'
 % (Convenient for development to avoid path conflicts -- one can backup a
 % copy with the same filename by saving it in any directory called old.) 
+cd(RAMPROOT)
 d = dir('**/old*');
 for i = 1:length(d)
   rmpath([d(i).folder '/' d(i).name]);
